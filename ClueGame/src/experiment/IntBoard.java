78c8c96b2940443ac76f4bce27f1551fffaca3fe
jtphan <jtphan@MZ026-25.adit.mines.edu>
2019-02-26 15:09:48 -0700

    Created calcAdjacencies() and getADjList()

--- ClueGame/src/experiment/IntBoard.java
+++ ClueGame/src/experiment/IntBoard.java
@@ -1,5 +1,16 @@
+//Authors: Michael Berg and Jennifer Phan
 package experiment;
+
+import java.util.Set;
 
 public class IntBoard {
 
+	public void calcAdjacencies() {
+		//calculates the adjacency list for each grid cell and stores the results as a Map in an inst. var.
+		
+	}
+	public Set<BoardCell> getAdjList() {
+		//Returns the adjacency list for one cell
+		return null;
+	}
 }
