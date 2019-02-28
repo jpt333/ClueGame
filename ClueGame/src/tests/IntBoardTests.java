38606ebbc70972059106239bba8cd60781ea8622
Jennifer Phan <jpt4542@gmail.com>
2019-02-28 14:49:29 -0700

    Import Test

--- ClueGame/src/tests/IntBoardTests.java
+++ ClueGame/src/tests/IntBoardTests.java
@@ -1,5 +1,17 @@
+//Authors: Michael Berg and Jennifer Phan
 package tests;
 
+
+
 public class IntBoardTests {
+<<<<<<< Upstream, based on origin/master
+=======
+
+	@Before
+	public void beforeAll() {
+		board = new IntBoard();
+	}
+	
+>>>>>>> 5660ec6 Import Test
 	
 }
