168364628adec79455feb496124308c582d54e6a
Michael Berg <gala52210@yahoo.com>
2019-02-28 14:39:44 -0700

    added board class

---
+++ ClueGame/src/clueGame/Board.java
@@ -0,0 +1,36 @@
+package clueGame;
+
+import java.util.Map;
+import java.util.Set;
+
+public class Board {
+	private int numRows;
+	private int numColumns;
+	public static final int MAX_BOARD_SIZE = 50;
+	
+	private BoardCell board [][];
+	
+	private Map<Character, String> legend;
+	
+	private Map<BoardCell, Set<BoardCell>> adjMatrix;
+	
+	private Set<BoardCell> targets;
+	
+	private String boardConfigFile;
+	private String roomConfigFile;
+	
+	public Board getInsrance() {
+		return this;
+	}
+	
+	public void initialize() {
+		
+	}
+	
+	public void loadRoomConfig() {
+		
+	}
+	
+	public void clac
+	
+}
