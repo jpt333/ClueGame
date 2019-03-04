9960616cb67bf55e608570adc7332c62520fa2b5
Jennifer Phan <jpt4542@gmail.com>
2019-03-04 12:03:57 -0700

    Added Adj. Test for cell (0,0) as in example

--- ClueGame/src/tests/IntBoardTests.java
+++ ClueGame/src/tests/IntBoardTests.java
@@ -1,7 +1,9 @@
 //Authors: Michael Berg and Jennifer Phan
 package tests;
 
+import java.util.Set;
 
+import clueGame.BoardCell;
 
 public class IntBoardTests {
 	
@@ -9,6 +11,13 @@ public class IntBoardTests {
 	public void beforeAll() {
 		board = new IntBoard();
 	}
-	
+	 @Test
+	 public void testAdjacency0() {
+		 BoardCell cell = board.getCell(0,0);
+		 Set<BoardCell> testList cell = board.getAdjList(cell);
+		 assertTrue(testList.contains(board.getCell(1, 0)));
+		 assertTrue(testList.contains(board.getCell(0, 1)));
+		 assertEquals(2, testList.size());
+	 }
 	
 }
