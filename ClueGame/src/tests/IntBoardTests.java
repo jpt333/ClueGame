74c06d00539a92a79b7105bf218e4379fc83d7e2
Jennifer Phan <jpt4542@gmail.com>
2019-03-04 13:12:53 -0700

    Deleted one of the tests

--- ClueGame/src/tests/IntBoardTests.java
+++ ClueGame/src/tests/IntBoardTests.java
@@ -106,18 +106,6 @@ public class IntBoardTests {
 		assertEquals(2, testList.size());
 	}
 	
-	@Test
-	public void testAdjacency0_3() {
-		BoardCell cell = board.getCell(0,0);
-		board.calcTargets(cell, 3);
-		Set<BoardCell> targets = board.getTargets();
-		assertEquals(6, targets.size());
-		assertTrue(targets.contains(board.getCell(3, 0)));
-		assertTrue(targets.contains(board.getCell(2, 1)));
-		assertTrue(targets.contains(board.getCell(0, 1)));
-		assertTrue(targets.contains(board.getCell(1, 2)));
-		assertTrue(targets.contains(board.getCell(0, 3)));
-		assertTrue(targets.contains(board.getCell(1, 0)));
-	}
+
 }
 
