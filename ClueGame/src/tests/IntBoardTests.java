06f7f1dee5385769633a68a91e5ad144399f68bc
Jennifer Phan <jpt4542@gmail.com>
2019-03-04 12:58:59 -0700

    Added a few tests in IntBoardTests

--- ClueGame/src/tests/IntBoardTests.java
+++ ClueGame/src/tests/IntBoardTests.java
@@ -12,7 +12,7 @@ public class IntBoardTests {
 		board = new IntBoard();
 	}
 	 @Test
-	 public void testAdjacency0() {
+	 public void testAdjacency0_0() {   //top left location
 		 BoardCell cell = board.getCell(0,0);
 		 Set<BoardCell> testList = board.getAdjList(cell);
 		 assertTrue(testList.contains(board.getCell(1, 0)));
@@ -24,7 +24,7 @@ public class IntBoardTests {
 	 public void testAdjacency0_3() {
 		 BoardCell cell = board.getCell(0,0);
 		 board.calcTargets(cell, 3);
-		 Set targets = board.getTargets();
+		 Set<BoardCell> targets = board.getTargets();
 		 assertEquals(6, targets.size());
 		 assertTrue(targets.contains(board.getCell(3, 0)));
 		 assertTrue(targets.contains(board.getCell(2, 1)));
@@ -33,6 +33,29 @@ public class IntBoardTests {
 		 assertTrue(targets.contains(board.getCell(0, 3)));
 		 assertTrue(targets.contains(board.getCell(1, 0)));
 		}
+	 
+	 @Test
+	 public void testAdjacency3_3() { //check bottom right corner
+		 BoardCell cell = board.getCell(3,3);
+		 board.calcTargets(cell, 1);
+		 Set<BoardCell> targets = board.getTargets();
+		 assertEquals(2, targets.size());
+		 assertTrue(targets.contains(board.getCell(3,2)));
+		 assertTrue(targets.contains(board.getCell(2,3)));
+	 }
+	 
+	 @Test
+	 public void testAdjacency1_3() { //check right edge
+		 BoardCell cell = board.getCell(1,3);
+		 board.calcTargets(cell, 1);
+		 Set<BoardCell> targets = board.getTargets();
+		 assertEquals(3, targets.size());
+		 assertTrue(targets.contains(board.getCell(2,3)));
+		 assertTrue(targets.contains(board.getCell(1,2)));
+		 assertTrue(targets.contains(board.getCell(0,3)));
+	 }
+	 
+	 
 		 
-	}
 }
+
