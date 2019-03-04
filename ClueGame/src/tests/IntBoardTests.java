a9194125312b363caa2717f74cdc247b9fc7860a
Jennifer Phan <jpt4542@gmail.com>
2019-03-04 13:12:10 -0700

    Little Changes

--- ClueGame/src/tests/IntBoardTests.java
+++ ClueGame/src/tests/IntBoardTests.java
@@ -17,7 +17,7 @@ public class IntBoardTests {
 	public void beforeAll() {
 		board = new IntBoard();
 	}
-<<<<<<< HEAD
+
 	 @Test
 	 public void testAdjacency0_0() {   //top left location
 		 BoardCell cell = board.getCell(0,0);
@@ -26,10 +26,7 @@ public class IntBoardTests {
 		 assertTrue(testList.contains(board.getCell(0, 1)));
 		 assertEquals(2, testList.size());
 	 }
-=======
->>>>>>> branch 'master' of https://github.com/michaelberg12/ClueGame.git
-	
-<<<<<<< HEAD
+
 	 @Test
 	 public void testAdjacency0_3() {
 		 BoardCell cell = board.getCell(0,0);
@@ -100,7 +97,6 @@ public class IntBoardTests {
 		 assertTrue(targets.contains(board.getCell(2,3)));
 	 }
 		 
-=======
 	@Test
 	public void testAdjacency0() {
 		BoardCell cell = board.getCell(0,0);
@@ -114,7 +110,7 @@ public class IntBoardTests {
 	public void testAdjacency0_3() {
 		BoardCell cell = board.getCell(0,0);
 		board.calcTargets(cell, 3);
-		Set targets = board.getTargets();
+		Set<BoardCell> targets = board.getTargets();
 		assertEquals(6, targets.size());
 		assertTrue(targets.contains(board.getCell(3, 0)));
 		assertTrue(targets.contains(board.getCell(2, 1)));
@@ -123,6 +119,5 @@ public class IntBoardTests {
 		assertTrue(targets.contains(board.getCell(0, 3)));
 		assertTrue(targets.contains(board.getCell(1, 0)));
 	}
->>>>>>> branch 'master' of https://github.com/michaelberg12/ClueGame.git
 }
 
