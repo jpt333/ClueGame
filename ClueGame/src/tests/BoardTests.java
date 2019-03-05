12d3a3caf466cc2a661f729dd04363439aa7b4f9
Jennifer Phan <jpt4542@gmail.com>
2019-03-05 14:46:02 -0700

    Updated correctInitial Test

--- ClueGame/src/tests/BoardTests.java
+++ ClueGame/src/tests/BoardTests.java
@@ -10,19 +10,12 @@ import clueGame.Board;
 
 class BoardTests {
 
-<<<<<<< HEAD
-	@Before
-	public void beforeAll() {
-		//calls in board
-	}
-=======
 	Board board;
 	
 	@Before
 	public void beforeAll() {
 		board = new Board();
 	}  
->>>>>>> branch 'master' of https://github.com/michaelberg12/ClueGame.git
 	
 	@Test
 	void doorDirection() {
@@ -43,10 +36,17 @@ class BoardTests {
 
 	@Test
 	public void correctInitial() { //checks whether the cells have the correct initial
-		assertEquals('C', board[0][0]);
-		assertEquals('W', board[5][0]);
-		assertEquals('B', board[1][7]);
-		assertEquals('X', board[8][12]);
+		assertEquals('B', board.getBoard()[0][0]);
+		assertEquals('W', board.getBoard()[4][8]);
+		assertEquals('P', board.getBoard()[0][10]);
+		assertEquals('X', board.getBoard()[1][22]);
+		assertEquals('L', board.getBoard()[6][23]);
+		assertEquals('K', board.getBoard()[11][22]);
+		assertEquals('D', board.getBoard()[16][14]);
+		assertEquals('C', board.getBoard()[8][5]);
+		assertEquals('A', board.getBoard()[13][1]);
+		assertEquals('G', board.getBoard()[3][14]);
+		assertEquals('M', board.getBoard()[12][13]);
 	}
 	
 	
