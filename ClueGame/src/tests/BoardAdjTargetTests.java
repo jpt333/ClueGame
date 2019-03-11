2efe43767ca8eb651a7add3dbfde6fb71014b5bd
Jennifer Phan <jpt4542@gmail.com>
2019-03-11 10:40:52 -0600

    Created new class to test adjacency

---
+++ ClueGame/src/tests/BoardAdjTargetTests.java
@@ -0,0 +1,15 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.Map;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import clueGame.Board;
+import clueGame.DoorDirection;
+
+public class BoardAdjTargetTests {
+	private static Board board;
+}
