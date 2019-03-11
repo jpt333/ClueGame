7d23e71495223135cd53fa0885255adf6a51a0e6
Jennifer Phan <jpt4542@gmail.com>
2019-03-11 10:44:12 -0600

    Added the @BeforeClass

--- ClueGame/src/tests/BoardAdjTargetTests.java
+++ ClueGame/src/tests/BoardAdjTargetTests.java
@@ -12,4 +12,16 @@ import clueGame.DoorDirection;
 
 public class BoardAdjTargetTests {
 	private static Board board;
+	
+	@BeforeClass
+	public static void setUp() {
+		// Board is singleton, get the only instance
+		board = Board.getInstance();
+		board.setConfigFiles("ClueBoard.csv", "rooms.txt");
+		// set the file names to use my config files
+		board.setConfigFiles("ClueBoard.csv", "rooms.txt");	
+		// Initialize will load BOTH config files 
+		board.initialize();
+	}
+	
 }
