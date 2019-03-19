121bccb21da0aeee5f7da9dc48654f8901927f06
Jennifer Phan <jpt4542@gmail.com>
2019-03-19 14:34:29 -0600

    Created GameSetupTests.java class to test for loading people, load/creating deck of cards, and dealing cards

---
+++ ClueGame/src/tests/GameSetupTests.java
@@ -0,0 +1,23 @@
+//Authors: Michael Berg and Jennifer Phan
+package tests;
+
+import static org.junit.Assert.*;
+
+import java.util.Set;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import clueGame.Board;
+import clueGame.BoardCell;
+
+//Test loading people, load/create deck of cards, and dealing cards
+public class GameSetupTests {
+	private static Board board;
+	
+	@BeforeClass
+	public static void setUp() {
+		
+	}
+	
+}
