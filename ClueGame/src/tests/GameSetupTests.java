59818d68876b6f902efb87d3d55d188d34b8d575
Jennifer Phan <jpt4542@gmail.com>
2019-03-19 15:01:33 -0600

    Started working on the GameSetupTests.java. Created few tests.

--- ClueGame/src/tests/GameSetupTests.java
+++ ClueGame/src/tests/GameSetupTests.java
@@ -13,11 +13,47 @@ import clueGame.BoardCell;
 
 //Test loading people, load/create deck of cards, and dealing cards
 public class GameSetupTests {
-	private static Board board;
+	public final int MAX_DECK_OF_CARDS = 21;
+	public final int MAX_ROOM_CARDS = 9;
+	public final int MAX_WEAPONS = 6;
+	public final int MAX_PERSON = 6;
 	
 	@BeforeClass
 	public static void setUp() {
 		
 	}
+	
+	//Checks correct total number of cards
+	@Test
+	public void testNumberOfCardsInDeck() {
+		assertEquals(24, /*TBD*/);
+	}
+	
+	//Checks correct number of each type of card
+	@Test
+	public void testTypeOfCard() {
+		if(/*PERSON*/) {
+			assertEquals(6, );
+		}
+		if(/*WEAPON*/) {
+			assertEquals(6, );
+		}
+		if(/*Room*/) {
+			assertEquals(9, );
+		}
+	}
+	
+	//Checks that there is a room, weapon, and person in deck
+	@Test
+	public void testHasCard() {
+		
+	}
+	
+	//Test that all players have roughly same number of cards after dealing
+	@Test
+	public void testDealingCards() {
+		//Also test that same card not given to more than 1 player
+	}
+	
 	
 }
