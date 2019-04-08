a0fd83f58d88e6e4b82a8f7ed2daa55ce2347ad1
Jennifer Phan <jpt4542@gmail.com>
2019-04-07 22:01:55 -0600

    Created Janel fore createBoard and Cards

--- ClueGame/src/clueGame/ClueGameGUI.java
+++ ClueGame/src/clueGame/ClueGameGUI.java
@@ -14,13 +14,41 @@ import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
 public class ClueGameGUI extends JPanel{
-	private JTextField name;
+	private JTextField people;
+	private JTextField rooms;
+	private JTextField weapons;
 	
 	public ClueGameGUI() {
-		setLayout(new GridLayout(2,0));
-		JPanel panel = createNamePanel();
+		setLayout(new GridLayout(3,0));
+		JPanel panel = createBoardAndCards();
 		add(panel);
-		panel = createButtonPanel();
+		panel = createTurnAndButtons();
 		add(panel);
+		panel = createRollAndGuessAndResult();
+		add(panel);
+	}
+	
+	private JPanel createBoardAndCards() {
+		Border compound;
+		JPanel content = new JPanel();
+		JPanel panel = new JPanel();
+		//Grid Layout, 1 row, 2 columns
+		panel.setLayout(new GridLayout(1,2));
+		JLabel nameBoard = new JLabel("Board");
+		panel.add(nameBoard);
+		content.add(panel);
+		people = new JTextField(20);
+		rooms = new JTextField(20);
+		weapons = new JTextField(20);
+		
+		panel = new JPanel();
+		panel.setBorder(new TitledBorder(new EtchedBorder(), "My Cards"));
+		compound = BorderFactory.createTitledBorder("People");
+		//Don't know how to add border inside border
+		panel.add(people);
+		panel.add(rooms);
+		panel.add(weapons);
+		content.add(panel);
+		return content;
 	}
 }
