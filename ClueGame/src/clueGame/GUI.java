9712f7c393f210684d8581740179987043b5d357
Jennifer Phan <jpt4542@gmail.com>
2019-04-07 21:57:05 -0600

    Created Gui class and imported necessary libraries

---
+++ ClueGame/src/clueGame/GUI.java
@@ -0,0 +1,18 @@
+package clueGame;
+
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.Border;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+
+public class GUI {
+
+}
