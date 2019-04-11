ea98f14d4304ef503d668b6882ff3c0309ec706d
Jennifer Phan <jpt4542@gmail.com>
2019-04-11 14:59:50 -0600

    Has checkboxes for each person

--- ClueGame/src/clueGame/DetectiveNotesDialog.java
+++ ClueGame/src/clueGame/DetectiveNotesDialog.java
@@ -1,13 +1,32 @@
 //Authors: Michael Berg and Jennifer Phan
 package clueGame;
 
+import java.awt.GridLayout;
+
+import javax.swing.JCheckBox;
 import javax.swing.JDialog;
 
 public class DetectiveNotesDialog extends JDialog{
-	public void DetectiveDialog() {
+	JCheckBox peoplee = new JCheckBox();
+	
+	public DetectiveNotesDialog() {
 		setTitle("Detective Dialog");
-		setSize(300,200);
+		setSize(300,300);
+		setLayout(new GridLayout(3,2));
+		//People
+		String[] people = new String[] {"Miss Scarlet", "Colonel Mustard", "Mr. Green", "Mrs. White", "Mrs. Peacock", "Professor Plum"};
+		for(String person : people) {
+			peoplee = new JCheckBox(person);
+			add(peoplee);
+		}
+		//Rooms
+		String[] rooms = new String[] {""};
+		
 	}
 	
-	
+	public static void main(String[] args) {
+		DetectiveNotesDialog dialog = new DetectiveNotesDialog();
+		
+		dialog.setVisible(true);
+	}
 }
