4b939a5bdf0d53f612c66731f8ab189f3430d969
Jennifer Phan <jpt4542@gmail.com>
2019-04-11 13:26:56 -0600

    Created DetectiveNotesDialog class

---
+++ ClueGame/src/clueGame/DetectiveNotesDialog.java
@@ -0,0 +1,13 @@
+//Authors: Michael Berg and Jennifer Phan
+package clueGame;
+
+import javax.swing.JDialog;
+
+public class DetectiveNotesDialog extends JDialog{
+	public void DetectiveDialog() {
+		setTitle("Detective Dialog");
+		setSize(300,200);
+	}
+	
+	
+}
