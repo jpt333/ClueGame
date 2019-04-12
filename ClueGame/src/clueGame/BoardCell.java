1505bb06273b91941eb76c6662c69dfbbd344eab
Jennifer Phan <jpt4542@gmail.com>
2019-04-11 22:27:45 -0600

    Started drawing the board

--- ClueGame/src/clueGame/BoardCell.java
+++ ClueGame/src/clueGame/BoardCell.java
@@ -1,6 +1,10 @@
 //Authors: Michael Berg and Jennifer Phan
 package clueGame;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Point;
+
 import clueGame.DoorDirection;
 
 public class BoardCell {
@@ -11,6 +15,14 @@ public class BoardCell {
 
 	private String initital;
 	private DoorDirection direction;
+	
+	private Point pixel;
+	private Color color;
+	
+	public final int WIDTH = 30;
+	public final int HEIGHT = 30;
+	public final int SCALE = 32;
+	private DoorDirection doorDir;
 	
 	public BoardCell(String initital, DoorDirection direction) {
 		this.initital = initital;
@@ -54,5 +66,37 @@ public class BoardCell {
 	
 	public void draw(Graphics g) {
 		//Draw rectangles and other things for board
+		if(initital == "W") {
+			color = Color.DARK_GRAY;
+			g.setColor(color);
+			g.fillRect(pixel.x, pixel.y, WIDTH, HEIGHT);
+		}
+		if(this.isRoom()) {
+			if(isDoorway()) {
+				if (doorDir == DoorDirection.UP)
+				{
+					color = Color.WHITE;
+					g.setColor(color);
+					g.fillRect(pixel.x, pixel.y, WIDTH, HEIGHT);
+					g.setColor(Color.BLUE);
+					g.fillRect(pixel.x, pixel.y, WIDTH, 5);
+				}
+				else if (doorDir == DoorDirection.DOWN)
+				{
+					
+				}
+				else if (doorDir == DoorDirection.LEFT)
+				{
+					
+				}
+				else if (doorDir == DoorDirection.RIGHT)
+				{
+					
+				}
+				else if(doorDir == DoorDirection.RIGHT) {
+					
+				}
+			}
+		}
 	}
 }
