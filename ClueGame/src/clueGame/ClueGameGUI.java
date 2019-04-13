378a3d4ce0246e676a4c893d1c5e9dc4ed7bcbb7
Michael Berg <urberg@yahoo.com>
2019-04-13 02:16:48 -0600

    added a fun loading class

--- ClueGame/src/clueGame/ClueGameGUI.java
+++ ClueGame/src/clueGame/ClueGameGUI.java
@@ -1,132 +1,225 @@
-//Authors: Michael Berg and Jennifer Phan
 package clueGame;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.GridLayout;
+import java.awt.EventQueue;
 
-import javax.swing.BorderFactory;
-import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
+import java.awt.Color;
+import java.awt.Toolkit;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.border.Border;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.EtchedBorder;
-import javax.swing.border.TitledBorder;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
+import javax.swing.JDesktopPane;
+import javax.swing.JTextPane;
+import javax.swing.SwingConstants;
+import java.awt.GridBagLayout;
+import javax.swing.BoxLayout;
+import net.miginfocom.swing.MigLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JEditorPane;
+import javax.swing.JTextArea;
+import java.awt.Dimension;
+import java.awt.SystemColor;
+import java.awt.Rectangle;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import java.awt.Component;
+import javax.swing.JMenuItem;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 
-public class ClueGameGUI extends JPanel{
-	private JTextField people;
-	private JTextField rooms;
-	private JTextField weapons;
-	
-	public ClueGameGUI() {
-		setLayout(new GridLayout(3,1));
-		JPanel panel = createBoardAndCards();
-		add(panel);
-		panel = createTurnAndButtons();
-		add(panel);
-		panel = createRollAndGuessAndResult();
-		add(panel);
-	}
-	
-	private JPanel createBoardAndCards() {
-		JPanel content = new JPanel();
-		JPanel panel = new JPanel();
-		//Grid Layout, 1 row, 2 columns
-		panel.setLayout(new GridLayout(1,3));
-		JLabel nameBoard = new JLabel("Board");
-		panel.add(nameBoard);
-		content.add(panel);
-		people = new JTextField(20);
-		rooms = new JTextField(20);
-		weapons = new JTextField(20);
-		
-		JPanel panel2 = new JPanel();
-		panel2.setBorder(new TitledBorder(new EtchedBorder(), "My Cards"));
-		panel2.setLayout(new GridLayout(3,0));
+public class ClueGameGUI {
 
-		people.setBorder(new CompoundBorder(BorderFactory.createTitledBorder("People"), people.getBorder()));
-		rooms.setBorder(new CompoundBorder(BorderFactory.createTitledBorder("Room"), rooms.getBorder()));
-		weapons.setBorder(new CompoundBorder(BorderFactory.createTitledBorder("Weapon"), weapons.getBorder()));
-		panel2.add(people, BorderLayout.NORTH);
-		panel2.add(rooms, BorderLayout.CENTER);
-		panel2.add(weapons, BorderLayout.SOUTH);
-		content.add(panel2, BorderLayout.EAST);
-		return content;
-	}
+	private JFrame clueWindow;
 	
-	private JPanel createTurnAndButtons() {
-		JPanel panel1 = new JPanel();
-		//1 row, 3 columns
-		panel1.setLayout(new GridLayout(1, 3));
-		
-		JPanel panel = new JPanel();
-		JLabel whoseTurn = new JLabel("Whose turn?");
-		panel.add(whoseTurn);
-		JTextField cardNamePlayer = new JTextField(20);
-		panel.add(cardNamePlayer);
-		panel1.add(panel);
-		panel = new JPanel();
-		JButton nextPlayer = new JButton("Next Player");
-		nextPlayer.setPreferredSize(new Dimension(250, 100));
-		JButton makeAccusation = new JButton("Make an accusation");
-		makeAccusation.setPreferredSize(new Dimension(250, 100));
-		panel.add(nextPlayer);
-		panel1.add(panel);
-		panel = new JPanel();
-		panel.add(makeAccusation);
-		panel1.add(panel);
-		return panel1;
-	}
+	//generated using window builder
 	
-	private JPanel createRollAndGuessAndResult() {
-		JPanel panel1 = new JPanel();
-		JLabel roll = new JLabel("Roll");
-		panel1.setLayout(new GridLayout(1, 3));
-		
-		
-		//Die Roll
-		JPanel panel = new JPanel();
-		panel.setBorder(new TitledBorder(new EtchedBorder(), "Die"));
-		panel.add(roll);
-		JTextField rollNum = new JTextField(20);
-		panel.add(rollNum);
-		panel1.add(panel);
-		
-		//Guess
-		panel = new JPanel();
-		panel.setBorder(new TitledBorder(new EtchedBorder(), "Guess"));
-		JLabel guess = new JLabel("Guess");
-		panel.add(guess);
-		JTextField guessPersonRoomWeapon = new JTextField(20);
-		panel.add(guessPersonRoomWeapon);
-		panel1.add(panel);
-		
-		//Guess Result Response
-		panel = new JPanel();
-		panel.setBorder(new TitledBorder(new EtchedBorder(), "Guess Result"));
-		JLabel response = new JLabel("Response");
-		panel.add(response);
-		JTextField responseText = new JTextField(20);
-		panel.add(responseText);
-		panel1.add(panel);
-		panel1.setPreferredSize(new Dimension(900, 100));
-		return panel1;
-	}
-	
+	/**
+	 * Launch the application.
+	 */
 	public static void main(String[] args) {
-		JFrame frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("Clue Game GUI");
-		frame.setSize(1500, 900);
-		frame.setSize(900,800);
-		ClueGameGUI gui = new ClueGameGUI();
-		frame.add(gui, BorderLayout.SOUTH);
-		frame.setVisible(true);
-		frame.setResizable(false);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					ClueGameGUI window = new ClueGameGUI();
+					window.clueWindow.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	
+	/**
+	 * Create the application.
+	 */
+	public ClueGameGUI() {
+		initialize();
 	}
 	
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		clueWindow = new JFrame();
+		clueWindow.setResizable(false);
+		clueWindow.getContentPane().setForeground(Color.WHITE);
+		clueWindow.getContentPane().setEnabled(false);
+		clueWindow.setForeground(Color.WHITE);
+		clueWindow.setIconImage(Toolkit.getDefaultToolkit().getImage(ClueGameGUI.class.getResource("/Resources/clueGameLogo.png")));
+		clueWindow.setTitle("Clue Game");
+		clueWindow.getContentPane().setBackground(Color.DARK_GRAY);
+		clueWindow.getContentPane().setLayout(null);
+		
+		JButton accusationButton = new JButton("Make an Accusation");
+		accusationButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		accusationButton.setBounds(673, 822, 158, 38);
+		clueWindow.getContentPane().add(accusationButton);
+		
+		JButton nextPlayer = new JButton("Next Player");
+		nextPlayer.setBounds(673, 758, 158, 38);
+		clueWindow.getContentPane().add(nextPlayer);
+		
+		JTextArea guessResult = new JTextArea();
+		guessResult.setForeground(Color.WHITE);
+		guessResult.setBackground(Color.DARK_GRAY);
+		guessResult.setText("Guess Result");
+		guessResult.setBounds(23, 681, 76, 16);
+		clueWindow.getContentPane().add(guessResult);
+		
+		JTextArea guess = new JTextArea();
+		guess.setText("Guess");
+		guess.setForeground(Color.WHITE);
+		guess.setBackground(Color.DARK_GRAY);
+		guess.setBounds(283, 681, 37, 16);
+		clueWindow.getContentPane().add(guess);
+		
+		JTextArea turnText = new JTextArea();
+		turnText.setText("Whose turn?");
+		turnText.setForeground(Color.WHITE);
+		turnText.setBackground(Color.DARK_GRAY);
+		turnText.setBounds(523, 681, 76, 16);
+		clueWindow.getContentPane().add(turnText);
+		
+		JTextArea myCards = new JTextArea();
+		myCards.setText("My Cards");
+		myCards.setForeground(Color.WHITE);
+		myCards.setBackground(Color.DARK_GRAY);
+		myCards.setBounds(728, 58, 51, 16);
+		clueWindow.getContentPane().add(myCards);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 994, 25);
+		clueWindow.getContentPane().add(menuBar);
+		
+		JMenu dropMenu = new JMenu("File");
+		menuBar.add(dropMenu);
+		
+		JMenuItem detectiveNotes = new JMenuItem("Detective Notes");
+		detectiveNotes.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				DetectiveNotesDialog dialog = new DetectiveNotesDialog();
+				dialog.setVisible(true);
+			}
+		});
+		dropMenu.add(detectiveNotes);
+		
+		JMenuItem quit = new JMenuItem("Quit");
+		quit.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				clueWindow.dispose();
+			}
+		});
+		dropMenu.add(quit);
+
+		JLabel guessResultCard = new JLabel("");
+		guessResultCard.setIcon(new ImageIcon(ClueGameGUI.class.getResource("/Resources/Proffesoor Plum.png")));
+		guessResultCard.setForeground(SystemColor.text);
+		guessResultCard.setBackground(SystemColor.window);
+		guessResultCard.setBounds(12, 722, 100, 150);
+		clueWindow.getContentPane().add(guessResultCard);
+		
+		JLabel weaponcard = new JLabel("");
+		weaponcard.setIcon(new ImageIcon(ClueGameGUI.class.getResource("/Resources/Knife.png")));
+		weaponcard.setForeground(Color.WHITE);
+		weaponcard.setBackground(Color.WHITE);
+		weaponcard.setBounds(143, 722, 100, 150);
+		clueWindow.getContentPane().add(weaponcard);
+		
+		JLabel personCard = new JLabel("");
+		personCard.setIcon(new ImageIcon(ClueGameGUI.class.getResource("/Resources/Mr. Green.png")));
+		personCard.setForeground(Color.WHITE);
+		personCard.setBackground(Color.WHITE);
+		personCard.setBounds(255, 722, 100, 150);
+		clueWindow.getContentPane().add(personCard);
+		
+		JLabel roomCard = new JLabel("");
+		roomCard.setIcon(new ImageIcon(ClueGameGUI.class.getResource("/Resources/PlaceHolder.png")));
+		roomCard.setForeground(Color.WHITE);
+		roomCard.setBackground(Color.WHITE);
+		roomCard.setBounds(367, 722, 100, 150);
+		clueWindow.getContentPane().add(roomCard);
+		
+		JLabel playTurnCard = new JLabel("");
+		playTurnCard.setIcon(new ImageIcon(ClueGameGUI.class.getResource("/Resources/Mrs White.png")));
+		playTurnCard.setForeground(Color.WHITE);
+		playTurnCard.setBackground(Color.WHITE);
+		playTurnCard.setBounds(507, 722, 100, 150);
+		clueWindow.getContentPane().add(playTurnCard);
+		
+		JLabel card1 = new JLabel("");
+		card1.setIcon(new ImageIcon(ClueGameGUI.class.getResource("/Resources/Mrs Scarlet.png")));
+		card1.setForeground(Color.WHITE);
+		card1.setBackground(Color.WHITE);
+		card1.setBounds(705, 96, 100, 150);
+		clueWindow.getContentPane().add(card1);
+		
+		JLabel card2 = new JLabel("");
+		card2.setIcon(new ImageIcon(ClueGameGUI.class.getResource("/Resources/Pipe.png")));
+		card2.setForeground(Color.WHITE);
+		card2.setBackground(Color.WHITE);
+		card2.setBounds(705, 271, 100, 150);
+		clueWindow.getContentPane().add(card2);
+		
+		JLabel card3 = new JLabel("");
+		card3.setIcon(new ImageIcon(ClueGameGUI.class.getResource("/Resources/Mrs Peacock.png")));
+		card3.setForeground(Color.WHITE);
+		card3.setBackground(Color.WHITE);
+		card3.setBounds(705, 447, 100, 150);
+		clueWindow.getContentPane().add(card3);
+	
+		
+		Board gameBoard = Board.getInstance();
+		gameBoard.setConfigFiles("ClueBoard.csv", "rooms.txt");
+		gameBoard.setCardFiles("weapons.txt", "Person.txt");
+		gameBoard.initialize();
+		gameBoard.setBounds(0, 25, 702, 676);
+		clueWindow.getContentPane().add(gameBoard);
+		
+		JTextArea diceText = new JTextArea();
+		diceText.setText("Dice Roll");
+		diceText.setForeground(Color.WHITE);
+		diceText.setBackground(Color.DARK_GRAY);
+		diceText.setBounds(728, 621, 51, 16);
+		clueWindow.getContentPane().add(diceText);
+		
+		JLabel diceIcon = new JLabel("");
+		diceIcon.setIcon(new ImageIcon(ClueGameGUI.class.getResource("/Resources/6.png")));
+		diceIcon.setBounds(719, 660, 75, 75);
+		clueWindow.getContentPane().add(diceIcon);
+		clueWindow.setBounds(100, 100, 848, 920);
+		clueWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+
 }
---
+++ ClueGame/src/clueGame/LoadingIntro.java
@@ -0,0 +1,98 @@
+package clueGame;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Color;
+import java.awt.Toolkit;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.JProgressBar;
+import java.awt.GridLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import com.jgoodies.forms.layout.RowSpec;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.SpringLayout;
+import java.awt.Component;
+import javax.swing.Box;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import java.awt.Label;
+import java.awt.Font;
+
+public class LoadingIntro extends JFrame {
+
+	public final int TIMEDURRATION = 2000;
+	
+	private JPanel contentPane;
+	private JProgressBar progressBar;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		LoadingIntro frame = new LoadingIntro();
+		frame.setVisible(true);
+		frame.update();
+		frame.dispose();
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoadingIntro() {
+		
+		final String FUNNYLINES[] = new String[9];
+		FUNNYLINES[0] = "Scrunching Scrunchies";
+		FUNNYLINES[1] = "Removing Stripes from Striped Horses";
+		FUNNYLINES[2] = "Optimizing the People";
+		FUNNYLINES[3] = "Solving World Hunger";
+		FUNNYLINES[4] = "Localizing";
+		FUNNYLINES[5] = "Reporting Bugs";
+		FUNNYLINES[6] = "Sock-Sock-Shoe-Shoe";
+		FUNNYLINES[7] = "Sock-Shoe-Sock-Shoe";
+		FUNNYLINES[8] = "Loading Next Loading Joke";
+		
+		Random rand = new Random();
+		int randomNum = rand.nextInt(FUNNYLINES.length);
+		
+		setResizable(false);
+		setIconImage(Toolkit.getDefaultToolkit().getImage(LoadingIntro.class.getResource("/Resources/clueGameLogo.png")));
+		setBackground(Color.DARK_GRAY);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.DARK_GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		progressBar = new JProgressBar(0, TIMEDURRATION);
+		progressBar.setBounds(100, 100, 250, 25);
+		contentPane.add(progressBar);
+		
+		Label label = new Label(FUNNYLINES[randomNum]);
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("Arial", Font.PLAIN, 20));
+		label.setAlignment(Label.CENTER);
+		label.setBounds(10, 149, 424, 35);
+		contentPane.add(label);
+	}
+	
+	public void update() {
+		long startTime = System.nanoTime();
+		while(TimeUnit.MILLISECONDS.convert(System.nanoTime() - startTime, TimeUnit.NANOSECONDS) < TIMEDURRATION) {
+			int time = (int) TimeUnit.MILLISECONDS.convert(System.nanoTime() - startTime, TimeUnit.NANOSECONDS);
+			
+			progressBar.setValue(time);
+			
+			progressBar.repaint();
+		}
+	}
+}
